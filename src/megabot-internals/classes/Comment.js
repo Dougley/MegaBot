146b55bb6d3612cbc2e382809cb90cbfc2c4011f
Remco <hello@dougley.com>
2018-11-11 14:51:40 +0100

    missing docs

--- src/megabot-internals/classes/Comment.js
+++ src/megabot-internals/classes/Comment.js
@@ -10,6 +10,11 @@ const Striptags = require('striptags')
  * @property {Date} updatedAt - The time at which the comment was last updated
  * @property {Object} sideloads - Any data you might have sideloaded
  * @property {String} rawContent - The raw content of the comment, be aware that this might include HTML tags
+ * @property {Number} authorId - The ID of the author of this comment
+ * @property {Number} postId - The ID of the post this comment belongs to
+ * @property {Boolean} official - Whether or not this comment is marked as official
+ * @property {Number} voteSum - The sum of all votes on the comment, can be negative
+ * @property {Number} voteCount - The total number of times this comment has been voted on
  */
 class Comment extends Base {
   constructor (props, data) {
