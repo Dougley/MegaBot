911b4a214207299c034201fe19b09bdfc5268f3b
Remco <hello@dougley.com>
2018-11-13 19:32:32 +0100

    catch internal server errors

--- src/megabot-internals/constants.js
+++ src/megabot-internals/constants.js
@@ -8,6 +8,9 @@ module.exports = {
       case 'Not Found' : {
         return "Using your input, no content could be found. Please make sure you haven't made a typo"
       }
+      case 'Internal Server Error': {
+        return "Zendesk didn't respond properly for whatever reason, please try again later.\nThis issue might be related to problems over at Zendesk, please check https://status.zendesk.com"
+      }
       default: {
         logger.error(e)
         return "Something went wrong, and I'm not exactly sure what, try again later"
