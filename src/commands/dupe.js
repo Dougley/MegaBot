2577171f7f1dbc862a6e03348502bc168f64cde9
Remco <hello@dougley.com>
2019-01-03 15:14:46 +0100

    alert if a dupe has already been submitted closes #19

--- src/commands/dupe.js
+++ src/commands/dupe.js
@@ -1,5 +1,6 @@
 const ZD = require('../megabot-internals/zendesk')
 const AQ = require('../megabot-internals/admin-queue')
+const DB = require('../databases/lokijs')
 
 module.exports = {
   meta: {
@@ -7,14 +8,19 @@ module.exports = {
   },
   fn: async (msg, suffix) => {
     const chunks = suffix.split(' ')
+    let dupe, target
     try {
-      const dupe = await ZD.getSubmission(MB_CONSTANTS.submissionRegex.test(chunks[0]) ? chunks[0].match(MB_CONSTANTS.submissionRegex)[1] : chunks[0])
-      const target = await ZD.getSubmission(MB_CONSTANTS.submissionRegex.test(chunks[1]) ? chunks[1].match(MB_CONSTANTS.submissionRegex)[1] : chunks[1])
-      if (dupe.id === target.id) return msg.channel.createMessage("Can't merge 2 of the same suggestions!")
-      msg.channel.createMessage('Dupe request submitted')
-      AQ.createMergeRequest(dupe, target, msg.author)
+      dupe = await ZD.getSubmission(MB_CONSTANTS.submissionRegex.test(chunks[0]) ? chunks[0].match(MB_CONSTANTS.submissionRegex)[1] : chunks[0])
+      target = await ZD.getSubmission(MB_CONSTANTS.submissionRegex.test(chunks[1]) ? chunks[1].match(MB_CONSTANTS.submissionRegex)[1] : chunks[1])
     } catch (e) {
-      msg.channel.createMessage(MB_CONSTANTS.generateErrorMessage(e))
+      return msg.channel.createMessage(MB_CONSTANTS.generateErrorMessage(e))
     }
+    if (dupe.id === target.id) return msg.channel.createMessage("Can't merge 2 of the same suggestions!")
+    if (DB.findSync('questions', {
+      'ids.target': target.id,
+      'ids.dupe': dupe.id,
+      type: 3
+    })) return msg.channel.createMessage('A dupe request for these suggestions was already submitted.')
+    AQ.createMergeRequest(dupe, target, msg.author).then(() => msg.channel.createMessage('Dupe request submitted'))
   }
 }
