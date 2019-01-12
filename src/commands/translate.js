cf941057782ce393b7e43974af8b765d7ac573fc
Remco <hello@dougley.com>
2019-01-12 13:03:46 +0100

    unlock translate command

--- src/commands/translate.js
+++ src/commands/translate.js
@@ -3,7 +3,7 @@ const TL = require('@vitalets/google-translate-api')
 
 module.exports = {
   meta: {
-    level: 2,
+    level: 1,
     timeout: 10
   },
   fn: async (msg, suffix) => {
@@ -11,6 +11,7 @@ module.exports = {
       const id = suffix.match(MB_CONSTANTS.submissionRegex) ? suffix.match(MB_CONSTANTS.submissionRegex)[1] : suffix
       const suggestion = await ZD.getSubmission(id, ['topics', 'users'])
       const translation = await TL(suggestion.cleanContent, { to: 'en' })
+      const title = await TL(suggestion.title, { to: 'en' })
       return msg.channel.createMessage({
         content: `Translated from \`${translation.from.language.iso}\``,
         embed: {
@@ -19,7 +20,7 @@ module.exports = {
             name: suggestion.sideloads.users[0].name,
             icon_url: suggestion.sideloads.users[0].photo ? suggestion.sideloads.users[0].photo.content_url : undefined
           },
-          title: suggestion.title.length > 250 ? suggestion.title.substring(0, 250) + '...' : suggestion.title,
+          title: title.length > 250 ? title.substring(0, 250) + '...' : title,
           description: translation.text.length === 0 ? '*No content*' : (translation.text.length > 1024 ? `${translation.text.slice(0, 990)}...\n*[Content has been cut off]*` : translation.text),
           url: suggestion.htmlUrl,
           timestamp: suggestion.createdAt,
