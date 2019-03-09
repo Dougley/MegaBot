909276adb32942608e751b78cd4de67e5adf4750
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2019-03-09 10:41:45 +0000

    Meme based hugs and fights (#72)

--- src/commands/fight.js
+++ src/commands/fight.js
@@ -15,7 +15,9 @@ const templates = [
   'A wild {r} appears!\n{r} uses Bite! It\'s not very effective...\n{s} uses Mega Punch! It\'s very effective!\n{s} has won!',
   'As {s} ran all sweaty and tired reaching out for a last punch, {r} dashed to the side, leaving {s} tumbling onto the ground.',
   '{s} tried to modify the Dupe Bomber 3000 to take down {r} with tons of dupe reports, but Dannysaur got there first and denied them all... Which broke the machine.',
-  '{s} Mega Evolved and tried to wipe out {r} with Hyper Beam! But {r} used Mimic and reversed it back onto {s} instead!'
+  '{s} Mega Evolved and tried to wipe out {r} with Hyper Beam! But {r} used Mimic and reversed it back onto {s} instead!',
+  '{s} threw a snowball at {r} but unfortunately it hits a window at Discord HQ. Oops',
+  '{s} tricked {r} into waking up the Sleeping Pizza. The Sleeping Pizza does not like being woken up, so it turned both {s} and {r} into Calzone Pizza. Rest In Pepperoni.'
 ]
 
 module.exports = {
