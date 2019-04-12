f91264256632376e2f545a0c06ae1ff67e7a6713
Remco <hello@dougley.com>
2019-04-12 20:10:49 +0200

    add any special states to info embed closes #96

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,5 +1,6 @@
 const ZD = require('../megabot-internals/zendesk')
 const inquire = require('../megabot-internals/controllers/inquirer')
+const DB = require('../databases/lokijs')
 
 module.exports = {
   meta: {
@@ -10,7 +11,29 @@ module.exports = {
   fn: (msg, suffix) => {
     const id = suffix.match(MB_CONSTANTS.submissionRegex) ? suffix.match(MB_CONSTANTS.submissionRegex)[1] : suffix
     if (MB_CONSTANTS.isID(id)) {
+      const reqs = DB.findSync('questions', {
+        $or: [{
+          type: 3,
+          'ids.dupe': parseInt(id)
+        }, {
+          type: 3,
+          'ids.target': parseInt(id)
+        }, {
+          type: 2,
+          zd_id: parseInt(id)
+        }]
+      })
+      const ddlete = DB.findSync('system', {
+        type: 'dupe-delete',
+        zd_id: parseInt(id)
+      })
       ZD.getSubmission(id, ['users', 'topics']).then(x => {
+        let str
+        if (reqs) {
+          if (reqs.type === 3) str = `This suggestion is ${reqs.ids.dupe === parseInt(id) ? `set to be merged into ${reqs.ids.target}` : `being targeted as a master for ${reqs.ids.dupe}`}`
+          else if (reqs.type === 2) str = 'This suggestion is marked for deletion'
+        } else if (ddlete) str = 'This suggestion is queued to be deleted due to a successful merge'
+        else str = 'This suggestion is normal' // required, cant leave embed fields unfinished
         return msg.channel.createMessage({
           embed: {
             color: 0x3498db,
@@ -40,6 +63,10 @@ module.exports = {
                 name: 'Comments',
                 value: x.commentCount,
                 inline: true
+              },
+              {
+                name: 'Additional info',
+                value: str
               }
             ]
           }
