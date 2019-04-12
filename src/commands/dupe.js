e8001bcbf8ff42068350f9f77d931c539422eefd
Remco <hello@dougley.com>
2019-04-12 20:19:45 +0200

    randomize order of dupe emotes

--- src/commands/dupe.js
+++ src/commands/dupe.js
@@ -28,7 +28,11 @@ module.exports = {
       ...generateEmbed(dupe, target)
     })
     await stall(2000)
-    INQ.awaitReaction([ID.emojis.dismiss, ID.emojis.confirm], x, msg.author.id).then(z => {
+    let emojis = [ID.emojis.dismiss, ID.emojis.confirm]
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value)
+    INQ.awaitReaction(emojis, x, msg.author.id).then(z => {
       if (z.id === ID.emojis.confirm.id) {
         AQ.createMergeRequest(dupe, target, msg.author).then((c) => {
           x.edit({ content: 'Dupe request submitted',
