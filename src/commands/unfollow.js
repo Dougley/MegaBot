4672da66a61f772f4da72235d067560f783881e0
blastoise186 <40033667+blastoise186@users.noreply.github.com>
2019-04-28 13:16:37 +0100

    Created an unfollow command
    This is a new command to unfollow or unsubscribe from posts on Zendesk

---
+++ src/commands/unfollow.js
@@ -0,0 +1,30 @@
+const ZD = require('../megabot-internals/zendesk')
+
+module.exports = {
+  meta: {
+    level: 1,
+    timeout: 5,
+    alias: ['unsubscribe']
+  },
+  fn: async (msg, suffix) => {
+    const id = suffix.match(MB_CONSTANTS.submissionRegex) ? suffix.match(MB_CONSTANTS.submissionRegex)[1] : suffix
+    try {
+      msg.channel.sendTyping()
+      const suggestion = await ZD.getSubmission(id)
+      await ZD.deleteSubscription(suggestion.id, msg.author.id)
+      msg.channel.createMessage({
+        content: "You're no longer subscribed to this suggestion, emails for it should stop soon.",
+        embed: {
+          title: suggestion.title,
+          url: suggestion.htmlUrl,
+          footer: {
+            text: `ID: ${suggestion.id}`
+          },
+          color: 0xe80404
+        }
+      })
+    } catch (e) {
+      return msg.channel.createMessage(MB_CONSTANTS.generateErrorMessage(e))
+    }
+  }
+}
