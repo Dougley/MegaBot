8e888a19c37f6a4c2555fb07c286deedde4d054e
Remco <hello@dougley.com>
2019-04-28 14:34:55 +0200

    add hyperlink to full results to search closes #100

--- src/commands/search.js
+++ src/commands/search.js
@@ -10,19 +10,21 @@ module.exports = {
     if (!suffix || suffix.length < 1) return msg.channel.createMessage('Enter a search term!')
     msg.channel.sendTyping()
     const data = await zd.searchSubmissions(suffix)
-    if (data.length > 0) msg.channel.createMessage(generateEmbed(data.slice(Math.max(data.length - 5, 0))))
+    if (data.length > 0) msg.channel.createMessage(generateEmbed(data.slice(Math.max(data.length - 5, 0)), suffix))
     else msg.channel.createMessage('Nothing found with your query!')
   }
 }
 
-const generateEmbed = (data) => {
+const generateEmbed = (data, suffix) => {
   return {
     content: 'Found the following suggestions',
     embed: {
       color: 0x008080,
-      description: data.map(x => `[${x.title}](${x.htmlUrl}) - ${x.id}`).join('\n'),
+      description: data.map(x => `[${x.title}](${x.htmlUrl}) - ${x.id}`).join('\n') +
+        '\n\n' +
+        `[Full results](${process.env.ZENDESK_ROOT_URL}/hc/en-us/search?${require('querystring').stringify({ query: suffix })})`,
       footer: {
-        text: 'Showing the top 5 results. Visit https://feedback.discordapp.com/ for the full results'
+        text: `Showing the top 5 results.`
       }
     }
   }
