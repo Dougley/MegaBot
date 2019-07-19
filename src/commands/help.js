c2429caa712f436cd6ebac51e5b1c8b785b89a65
MoonlightCapital <supertoad65@yahoo.com>
2019-07-19 16:44:44 +0200

    Help command

---
+++ src/commands/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+  meta: {
+    level: 0
+  },
+  fn: (msg) => {
+    msg.channel.createMessage(`
+Looking to submit your own suggestion to our feedback site? Check out <#268812893087203338> for a detailed guide! If you’re having trouble, please reach out in any of the suggestion channels and a Custodian will help you out as soon as they’re able!
+    `)
+  }
+}
