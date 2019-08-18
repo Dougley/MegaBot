395a760f5a0449973b7486e553ffceedf2f4fafd
Ed Luff <beartechtalks@gmail.com>
2019-08-18 11:52:36 +0100

    update !tr error message

--- src/commands/translate.js
+++ src/commands/translate.js
@@ -35,7 +35,7 @@ module.exports = {
         }
       }).then(x => inquire.createChatvote(x, suggestion.id))
     } catch (e) {
-      return msg.channel.createMessage(MB_CONSTANTS.generateErrorMessage(e))
+      return msg.channel.createMessage("This post is either being held for review or has been removed, or Google Translate broke.")
     }
   }
 }
