3bcb5c09d1766f855bf1f20e00e77831304f9e8a
Remco <hello@dougley.com>
2019-09-01 14:39:48 +0200

    rewrite reverse command for comment support closes #177

--- src/commands/reverse.js
+++ src/commands/reverse.js
@@ -4,17 +4,33 @@ module.exports = {
   meta: {
     level: 2
   },
-  fn: (msg, suffix) => {
-    let id = (MB_CONSTANTS.submissionRegex.test(suffix)) ? suffix.match(MB_CONSTANTS.submissionRegex)[1] : suffix
-    ZD.getSubmission(id).then(x => {
-      return ZD.getUser(x.authorId)
-    }).then(async x => {
-      const user = x.external_id ? await bot.getRESTUser(x.external_id) : null
-      if (!user) return msg.channel.createMessage('Somehow failed to pull information on this user :(')
-      msg.channel.createMessage(generateEmbed(user, x))
-    }).catch(e => {
-      msg.channel.createMessage(MB_CONSTANTS.generateErrorMessage(e))
-    })
+  fn: async (msg, suffix) => {
+    let id = MB_CONSTANTS.determineID(suffix)
+    if (Array.isArray(id)) {
+      const resp = await msg.channel.createMessage('Resolving a comment author, hold on...')
+      try {
+        const comment = await ZD.getComment(id[0], id[1])
+        const zduser = await ZD.getUser(comment.authorId)
+        const duser = await bot.getRESTUser(zduser.external_id)
+        await resp.edit({
+          embed: generateEmbed(duser, zduser)
+        })
+      } catch (e) {
+        await resp.edit('Something went wrong! :(')
+      }
+    } else {
+      const resp = await msg.channel.createMessage('Resolving a suggestion author, hold on...')
+      try {
+        const suggestion = await ZD.getSubmission(id)
+        const zduser = await ZD.getUser(suggestion.authorId)
+        const duser = await bot.getRESTUser(zduser.external_id)
+        await resp.edit({
+          embed: generateEmbed(duser, zduser)
+        })
+      } catch (e) {
+        await resp.edit('Something went wrong! :(')
+      }
+    }
   }
 }
 
