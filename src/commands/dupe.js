5a500d5126f3f865bab804d1feee022c85b0aee9
Kenji Adeini <54913577+Celeron64@users.noreply.github.com>
2019-09-05 19:54:22 -0700

    Check for duplicates which have been submitted by the author

--- src/commands/dupe.js
+++ src/commands/dupe.js
@@ -20,6 +20,7 @@ module.exports = {
       const dupes = await Promise.all(chunks.map(x => ZD.getSubmission(MB_CONSTANTS.determineID(x), ['users', 'topics'])))
       if (dupes.some(x => x.status === 'answered')) return message.edit('Some of your dupes are marked as answered, you cannot merge those')
       if (dupes.some(x => DB.findSync('questions', { 'ids.dupe': x.id, type: 3 }))) return message.edit('One or more of your dupes may have been submitted by yourself or another Custodian. Please double check the IDs and try again.')
+      if (dupes.some(x => x.authorId === msg.author.id)) return msg.edit('One or more of your dupes have been submitted by you, please double check the IDs and try again.')
       await message.edit({
         content: 'Is this correct?',
         ...generateEmbed(dupes, target)
